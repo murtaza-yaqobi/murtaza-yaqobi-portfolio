bd2856392055bcf20ad016de683a97ccda6bece4
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-08-14 18:31:53 +0430

    |

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -13,7 +13,7 @@ export default function Skills() {
     // "Responsive Design",
     // "Responsive Design",
     // "Responsive Design",
-    // "Responsive Design",
+
   ];
   return (
     <section id="skills" className="bg-gray-100 py-20">
