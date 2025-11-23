791a2d0cc87d90c7045f8097472671f09cb3138b
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-11-23 07:02:03 +0430

    L added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,6 +2,11 @@ export default function Footer() {
   return (
     <footer className="bg-white py-6 text-xl text-center">
       
+
+
+      <p>
+        © 2025 <strong>Murtaza Yaqobi</strong>. All rights reserved.
+      </p>
     </footer>
   );
 }
