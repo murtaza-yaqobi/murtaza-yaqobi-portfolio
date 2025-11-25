d3983b9928e26368af0bd4ec4a9a5b232be4cb00
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-11-25 07:05:48 +0430

    Pushed files

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,9 +24,7 @@ export default function About() {
         </div>
       </div>
       <div>
-        <h1>Hello, wrold!</h1>
-        <h1>Hello, wrold!</h1>
-        <h1>Hello, wrold!</h1>
+        
       </div>
     </section>
   );
