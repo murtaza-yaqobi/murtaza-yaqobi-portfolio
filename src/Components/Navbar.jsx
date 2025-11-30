d6ddf07e2be2c8b5860f94bf1138be276a2c63e2
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2025-11-30 07:18:39 +0430

    |

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,8 +14,6 @@ export default function Navbar() {
             "skills",
             "projects",
             "contact",
-            "contact",
-            "contact",
           ].map((link) => (
             <a
               key={link}
