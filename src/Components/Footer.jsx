d7d8d426597ae7c1d233c6bc0126ac4fa0fb100c
murtaza-yaqobi <murtazayaqobi7@gmail.com>
2026-01-01 05:58:55 +0430

    O

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,9 +4,6 @@ export default function Footer() {
       <p>
         © 2025 <strong>Murtaza Yaqobi</strong>. All rights reserved.
       </p>
-      <h1>HI</h1>
-     
-     <h1>Hello Boys</h1>
     </footer>
     
   );
